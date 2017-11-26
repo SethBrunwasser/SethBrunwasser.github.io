444616afa06a6ad11b6538a79bcf6ef1a2df58e4
Seth Brunwasser <sbrunwasser1998@gmail.com>
2017-11-25 21:09:53 -0700

    realestate.html
    RoR JQuery template

--- realestate.js
+++ realestate.js
@@ -1,1 +1,25 @@
 // Real Estate Analysis calculator
+
+$(document).ready(function(){
+	$("input").change(function(){
+			$("#ror-10").val("ror 10 works");
+			$("#ror-20").val("ror 20 works");
+			$("#ror-30").val("ror 30 works");
+	})
+})
+
+//Calculate RoR - 10 Years
+function TenRoR(){
+
+}
+
+
+//Calculate RoR - 20 Years
+function TwentyRoR(){
+
+}
+
+//Calculate RoR - 30 Years
+function ThirtyRoR(){
+	
+}
