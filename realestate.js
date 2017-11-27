bf60edaa1af54dca776995fb0aa2d54453ae9ebc
Seth Brunwasser <sbrunwasser1998@gmail.com>
2017-11-26 18:08:01 -0700

    caprate function
    caprate function

--- realestate.js
+++ realestate.js
@@ -2,16 +2,49 @@
 
 $(document).ready(function(){
 	$("input").change(function(){
-			$("#ror-10").val(tenRoR());
+			$("#cap-rate").val(yearOneCapRate());
+			$("#ror-10").val(TenRoR());
 			$("#ror-20").val(TwentyRoR());
 			$("#ror-30").val(ThirtyRoR());
 	})
 })
 
+//Get value from id
+function getInput(id) {
+	var val = $(id).val();
+	if(val){
+		var num = parseInt(val);
+	} else {
+		var num = 0;
+	}
+	return num;
+}
+
+//Return Total Operating Income
+function TotalOperatingIncome(){
+	var operatingIncome = 12*getInput("#monthly-rent");
+	return operatingIncome;
+}
+
+//Return Total Operating Expenses
+function TotalOperatingExpenses(){
+	var monthlyToe = getInput("#electricity") + getInput("#water-sewer")
+				+ getInput("#garbage") + getInput("#hoas")
+				+ getInput("#insurance") + getInput("#management");
+	var monthlyPropertyTax = (getInput("#price") * getInput("#property-taxes")) / 12;
+	return monthlyToe + monthlyPropertyTax;
+}
+
+//Return Cap Rate
+function yearOneCapRate(){
+	return (TotalOperatingIncome()-TotalOperatingExpenses())/TotalOperatingExpenses();
+}
+
+
 //Calculate RoR - 10 Years
 function TenRoR(){
-
-	return "";
+	price = getInput("#price")
+	return price;
 }
 
 
