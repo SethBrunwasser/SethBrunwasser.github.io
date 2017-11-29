979368d207e5c230d6c18b7bb1c3b79838e339dd
Seth Brunwasser <sbrunwasser1998@gmail.com>
2017-11-28 22:41:47 -0700

    function calls
    Efficient function calls and formatting for currency and percentage

--- realestate.js
+++ realestate.js
@@ -2,11 +2,12 @@
 
 $(document).ready(function(){
 	$("input").change(function(){
-			$("#cap-rate").val(yearOneCapRate());
-			$("#Cash-on-Cash").val(yearOneCashOnCash())
-			$("#ror-10").val(TenRoR());
-			$("#ror-20").val(TwentyRoR());
-			$("#ror-30").val(ThirtyRoR());
+			$("#cap-rate").val(yearOneCapRate.formatPercent());
+			$("#Cash-on-Cash").val(yearOneCashOnCash.formatPercent())
+			$("#equity-5").val(equity(5).formatMoney(2));
+			$("#ror-10").val(TenRoR().formatPercent());
+			$("#ror-20").val(TwentyRoR().formatPercent());
+			$("#ror-30").val(ThirtyRoR().formatPercent());
 	})
 })
 
@@ -15,7 +16,7 @@ function getInput(id) {
 	var val = $(id).val();
 	if(val){
 		if(val.indexOf("%") > -1){
-			val = val.replace("%", "");
+			var val = val.replace("%", "");
 		}
 		if(val.indexOf("$") > -1){
 			val = val.replace("$", "");
@@ -30,83 +31,73 @@ function getInput(id) {
 	return num;
 }
 
-//Return Property Value
-function propertyValue(){
-	return getInput("#after-repair-value");
+Number.prototype.formatMoney = function(c, d, t){
+var n = this, 
+    c = isNaN(c = Math.abs(c)) ? 2 : c, 
+    d = d == undefined ? "." : d, 
+    t = t == undefined ? "," : t, 
+    s = n < 0 ? "-" : "", 
+    i = String(parseInt(n = Math.abs(Number(n) || 0).toFixed(c))), 
+    j = (j = i.length) > 3 ? j % 3 : 0;
+   return s + (j ? i.substr(0, j) + t : "") + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + t) + (c ? d + Math.abs(n - i).toFixed(c).slice(2) : "");
+ };
+
+Number.prototype.formatPercent = function(){
+	var n = this;
+	return (n * 100).toString().concat("%");
 }
 
-function purchasePrice(){
-	var price = getInput("#price");
-	return price;
-}
 
-//Return Down Payment
-function downPayment(){
-	var downpayment = purchasePrice() * (getInput("#downpayment") / 100);
-	return downpayment;
-}
-
-function loanLength(){
-	var length = getInput("#amortized-length");
-	return length;
-}
-
+//Input variables
+var propertyValue = function() {return getInput("#after-repair-value");}
+var purchasePrice = function() {return getInput("#price");}
+var downPayment = function() {return getInput("#downpayment")/100;}
+var loanLength = function() {return getInput("#amortized-length");}
+var loanInterestRate = function() {return getInput("#loan-interest-rate");}
+var closingCosts = function() {return getInput("#closing-costs");}
+var monthlyRent = function() {return getInput("#monthly-rent");}
+var pvGrowth = function() {return getInput("#annual-pv-growth");}
 //Return Monthly Mortgage
-function monthlyMortgage(){
-	var numOfPayments = loanLength()*12;
-	var rate = getInput("#loan-interest-rate")/(numOfPayments * 100);
+var monthlyMortgage = function() {
+	var numOfPayments = loanLength()*12*100;
+	var rate = loanInterestRate()/numOfPayments;
 	var principle = purchasePrice() - downPayment();
 	var monthlyMortgagePayment = principle * ((rate * Math.pow(1+rate, numOfPayments))/(Math.pow(1+rate, numOfPayments)-1));
-
-	console.log(monthlyMortgagePayment);
-
 	return monthlyMortgagePayment;
 }
 
 //Return Initial Investment
 function initialInvestment(){
-	var initialInvestment = downPayment() + getInput("#closing-costs");
+	var initialInvestment = downPayment() + closingCosts();
 	return initialInvestment;
 }
 
-//Return Total Operating Income
-function TotalMonthlyOperatingIncome(){
-	var operatingIncome = getInput("#monthly-rent");
-	return operatingIncome;
-}
-
 //Return Total Operating Expenses
-function TotalMonthlyOperatingExpenses(){
-	var monthlyToe = getInput("#electricity") + getInput("#water-sewer")
+var monthlyOperatingExpenses = function() {var monthlyToe = getInput("#electricity") + getInput("#water-sewer")
 				+ getInput("#garbage") + getInput("#hoas")
 				+ getInput("#insurance") + getInput("#management");
-	var monthlyPropertyTax = (getInput("#after-repair-value") * getInput("#property-taxes")) / 12;
-	return monthlyToe + monthlyPropertyTax;
-}
+				var monthlyPropertyTax = (getInput("#after-repair-value") * getInput("#property-taxes")) / 12;
+				return monthlyToe + monthlyPropertyTax;}
 
 //Return Net Operating Income
-function netMonthlyOperatingIncome(){
-	return TotalMonthlyOperatingIncome() - TotalMonthlyOperatingExpenses();
-}
+var netMonthlyOperatingIncome = function() { return monthlyRent() - monthlyOperatingExpenses();}
 
 //Return Cashflow
-function monthlyCashFlow(){
-	var monthlyCashflow = netMonthlyOperatingIncome() - monthlyMortgage();
-	return monthlyCashflow;
-}
+var monthlyCashFlow = function(){ return netMonthlyOperatingIncome() - monthlyMortgage();}
 
 //Return Year One Cash on Cash Return
-function yearOneCashOnCash(){
-	var yearOneCashOnCash = monthlyCashFlow()/initialInvestment();
-	return yearOneCashOnCash;
-}
+var yearOneCashOnCash = function(){ return monthlyCashFlow()/initialInvestment();}
 
 //Return Cap Rate
-function yearOneCapRate(){
-	return netMonthlyOperatingIncome()/propertyValue();
+var yearOneCapRate = function(){return netMonthlyOperatingIncome()/propertyValue();}
+
+//equity calculator
+function equity(year){
+	//Property Value * (1 + Appreciation Rate)^year
+	var rate = pvGrowth() / 100;
+	var equityOneYear = propertyValue() * Math.pow(1 + rate, year);
+	return equityOneYear;
 }
-
-
 
 //Calculate RoR - 10 Years
 function TenRoR(){
